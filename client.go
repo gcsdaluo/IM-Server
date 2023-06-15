12b189dbb1383d96bc5b07b84ab43b0fff6da44c
daluo <gcsdaluo@126.com>
2023-06-15 16:16:25 +0800

    客户端-更新用户名封装

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 // net.Conn 是 Go 语言中用于表示通用网络连接的接口类型。
@@ -38,6 +40,12 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+// Dealresponse 处理server回应的消息，直接显示到标准输出即可
+func (client *Client) Dealresponse() {
+	// 一旦client.conn有数据，就直接copy到stdout标准输出上，永久阻塞监听
+	io.Copy(os.Stdout, client.conn)
+}
+
 func (client *Client) menu() bool {
 	var flag int
 
@@ -57,6 +65,21 @@ func (client *Client) menu() bool {
 	}
 }
 
+func (client *Client) UpdateName() bool {
+
+	fmt.Println(">>>>请输入用户名:")
+	fmt.Scanln(&client.Name)
+
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return false
+	}
+
+	return true
+}
+
 func (client *Client) Run() {
 	for client.flag != 0 {
 		for client.menu() != true {
@@ -74,7 +97,7 @@ func (client *Client) Run() {
 			fmt.Println("公聊模式选择...")
 		case 3:
 			//更新用户名
-			fmt.Println("更新用户名选择...")
+			client.UpdateName()
 
 		}
 	}
@@ -100,6 +123,9 @@ func main() {
 		fmt.Println(">>>>> 链接服务器失败...")
 		return
 	}
+
+	// 单独开启一个goroutine去处理server的
+	go client.Dealresponse()
 
 	fmt.Println(">>>>> 链接服务器成功...")
 
