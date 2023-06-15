64505adffa84e78ab40811e2e15f8c94849f61bc
daluo <gcsdaluo@126.com>
2023-06-15 11:42:31 +0800

    超时强踢功能

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -62,6 +63,9 @@ func (this *Server) Handler(conn net.Conn) {
 
 	user.Online()
 
+	//监听用户是否活跃的channel
+	isLive := make(chan bool)
+
 	// 接收客户端发送的消息
 	go func() {
 		buf := make([]byte, 4096)
@@ -83,11 +87,36 @@ func (this *Server) Handler(conn net.Conn) {
 
 			//用户针对msg进行消息的处理
 			user.DoMessage(msg)
+
+			// 用户的任意消息，代表当前用户是一个活跃的
+			isLive <- true
 		}
 	}()
 
 	// 当前handler阻塞
-	select {}
+	for {
+		select {
+		case <-isLive:
+			// 当前用户是活跃的，应该重置定时器
+			// 不做任何事情，为了激活select，更新下面的定时器
+		case <-time.After(time.Second * 10):
+			// 已经超时 将当前User强制的关闭
+
+			user.SendMsg("你被踢了")
+
+			// 销毁用的资源
+			close(user.C)
+
+			//关闭链接
+			conn.Close()
+
+			//退出当前handler
+			return
+
+		}
+
+	}
+
 }
 
 // Start 启动服务器的接口
